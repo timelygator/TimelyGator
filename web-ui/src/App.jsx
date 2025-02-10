bb0318a9c6b88db7755fc621256d6aee1095f9cc
YashDVerma <99968246+YashDVerma@users.noreply.github.com>
2025-02-09 23:39:01 -0500

    aplied background light mode

--- web-ui/src/App.jsx
+++ web-ui/src/App.jsx
@@ -12,10 +12,10 @@ import SettingsPage from "./pages/SettingsPage";
 
 function App() {
 	return (
-		<div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+		<div className='flex h-screen bg-gray-300 text-gray-100 overflow-hidden dark:bg-gray-900 '>
 			{/* BG */}
 			<div className='fixed inset-0 z-0'>
-				<div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
+				{/* <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' /> */}
 				<div className='absolute inset-0 backdrop-blur-sm' />
 			</div>
 
