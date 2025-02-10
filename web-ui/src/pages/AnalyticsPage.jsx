78434d48852028d33d6f712e6f57dd372a8165ab
YashDVerma <99968246+YashDVerma@users.noreply.github.com>
2025-02-10 01:13:57 -0500

    fixed certain pages to migrating to different theme

--- web-ui/src/pages/AnalyticsPage.jsx
+++ web-ui/src/pages/AnalyticsPage.jsx
@@ -10,7 +10,7 @@ import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
 
 const AnalyticsPage = () => {
 	return (
-		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+		<div className='flex-1 overflow-auto relative z-10'>
 			<Header title={"Analytics Dashboard"} />
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
