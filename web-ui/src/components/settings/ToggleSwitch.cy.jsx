2bef21763336babe8dcb1e3ca8d9dac9900793d8
PulkitGarg777 <pulkitgarg777@gmail.com>
2025-03-02 19:14:07 -0500

    test: add Cypress tests for ToggleSwitch component and improve toggle functionality

--- web-ui/src/components/settings/ToggleSwitch.cy.jsx
+++ web-ui/src/components/settings/ToggleSwitch.cy.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import ToggleSwitch from './ToggleSwitch'
+import React from 'react';
+import ToggleSwitch from './ToggleSwitch';
 
 describe('<ToggleSwitch />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<ToggleSwitch />)
-  })
-})
+    cy.mount(<ToggleSwitch label="Test Toggle" isOn={false} onToggle={() => {}} />);
+  });
+
+  it('toggles the switch', () => {
+    const onToggleSpy = cy.spy().as('onToggleSpy');
+    cy.mount(<ToggleSwitch label="Test Toggle" isOn={false} onToggle={onToggleSpy} />);
+
+    // Select the toggle switch using the data-cy attribute
+    cy.get('[data-cy=toggle-switch]').as('toggleSwitch');
+
+    // Verify initial state
+    cy.get('@toggleSwitch').should('have.class', 'bg-gray-600');
+
+    // Click to toggle on
+    cy.get('@toggleSwitch').click();
+    cy.get('@toggleSwitch').should('have.class', 'bg-indigo-600');
+    cy.get('@onToggleSpy').should('have.been.calledOnce');
+
+    // Click to toggle off
+    cy.get('@toggleSwitch').click();
+    cy.get('@toggleSwitch').should('have.class', 'bg-gray-600');
+    cy.get('@onToggleSpy').should('have.been.calledTwice');
+  });
+});
--- web-ui/src/components/settings/ToggleSwitch.jsx
+++ web-ui/src/components/settings/ToggleSwitch.jsx
@@ -1,21 +1,32 @@
+import React, { useState } from 'react';
+
 const ToggleSwitch = ({ label, isOn, onToggle }) => {
-	return (
-		<div className='flex items-center justify-between py-3'>
-			<span className='text-gray-300'>{label}</span>
-			<button
-				className={`
+    const [isToggled, setIsToggled] = useState(isOn);
+
+    const handleToggle = () => {
+        setIsToggled(!isToggled);
+        onToggle(!isToggled);
+    };
+
+    return (
+        <div className='flex items-center justify-between py-3'>
+            <span className='text-gray-300'>{label}</span>
+            <button
+                data-cy="toggle-switch"
+                className={`
         relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none
-        ${isOn ? "bg-indigo-600" : "bg-gray-600"}
+        ${isToggled ? "bg-indigo-600" : "bg-gray-600"}
         `}
-				onClick={onToggle}
-			>
-				<span
-					className={`inline-block size-4 transform transition-transform bg-white rounded-full 
-            ${isOn ? "translate-x-6" : "translate-x-1"}
+                onClick={handleToggle}
+            >
+                <span
+                    className={`inline-block w-4 h-4 transform transition-transform bg-white rounded-full 
+            ${isToggled ? "translate-x-6" : "translate-x-1"}
             `}
-				/>
-			</button>
-		</div>
-	);
+                />
+            </button>
+        </div>
+    );
 };
+
 export default ToggleSwitch;
