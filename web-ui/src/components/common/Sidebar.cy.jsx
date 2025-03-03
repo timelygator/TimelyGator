9b29795e0af6f8887e3d1eaa0cdb06e24ea3036e
PulkitGarg777 <pulkitgarg777@gmail.com>
2025-03-02 19:19:18 -0500

    test: add Cypress tests for Sidebar component and implement collapse functionality

--- web-ui/src/components/common/Sidebar.cy.jsx
+++ web-ui/src/components/common/Sidebar.cy.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import Sidebar from './Sidebar'
+import React from 'react';
+import Sidebar from './Sidebar';
+import { BrowserRouter as Router } from 'react-router-dom'; // Assuming you're using react-router
 
 describe('<Sidebar />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Sidebar />)
-  })
-})
+    cy.mount(
+      <Router>
+        <Sidebar />
+      </Router>
+    );
+  });
+
+  it('collapses the sidebar on click', () => {
+    cy.mount(
+      <Router>
+        <Sidebar />
+      </Router>
+    );
+
+    // Select the collapse button using the data-cy attribute
+    cy.get('[data-cy=collapse-button]').as('collapseButton');
+
+    // Verify initial state (expanded)
+    cy.get('@collapseButton').should('exist');
+    cy.get('[data-cy=sidebar]').should('have.class', 'w-64');
+
+    // Click to collapse
+    cy.get('@collapseButton').click();
+    cy.get('[data-cy=sidebar]').should('have.class', 'w-20');
+
+    // Click to expand again
+    cy.get('@collapseButton').click();
+    cy.get('[data-cy=sidebar]').should('have.class', 'w-64');
+  });
+});
