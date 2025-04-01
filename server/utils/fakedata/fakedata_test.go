b7042b352b34ffce31d574d5281ba0881f34037c
Siddhant Chauhan <chauhanjoy10@gmail.com>
2025-03-31 23:15:08 -0400

    test: add unit tests for sameDay and weightedChoice functions in fakedata utility

--- server/utils/fakedata/fakedata_test.go
+++ server/utils/fakedata/fakedata_test.go
@@ -25,3 +25,37 @@ func TestParseDateFlag(t *testing.T) {
 	}
 }
 
+func TestSameDay(t *testing.T) {
+	date1 := time.Date(2024, 3, 31, 12, 0, 0, 0, time.UTC)
+	date2 := time.Date(2024, 3, 31, 23, 59, 59, 0, time.UTC)
+	date3 := time.Date(2024, 4, 1, 0, 0, 0, 0, time.UTC)
+
+	if !sameDay(date1, date2) {
+		t.Errorf("sameDay failed, expected true for same dates")
+	}
+	if sameDay(date1, date3) {
+		t.Errorf("sameDay failed, expected false for different dates")
+	}
+}
+
+func TestWeightedChoice(t *testing.T) {
+	items := []sampleData{
+		{App: "test1", Weight: 0},
+		{App: "test2", Weight: 1},
+		{App: "test3", Weight: 99},
+	}
+
+	counts := map[string]int{"test1": 0, "test2": 0, "test3": 0}
+	iterations := 1000
+	for i := 0; i < iterations; i++ {
+		choice := weightedChoice(items)
+		counts[choice.App]++
+	}
+
+	if counts["test1"] != 0 {
+		t.Errorf("Expected 'test1' to never be chosen, but got %d occurrences", counts["test1"])
+	}
+	if counts["test3"] <= counts["test2"] {
+		t.Errorf("Expected 'test3' to be chosen significantly more than 'test2', got test3: %d, test2: %d", counts["test3"], counts["test2"])
+	}
+}
