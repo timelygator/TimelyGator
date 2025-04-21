f21a9d85f32c1bbd09d56b4fafd77ca58e85202c
Shreyansh Nayak <195820240+shreyansh-nayak-ufl@users.noreply.github.com>
2025-04-21 15:54:50 -0400

    fix: add lib jxa path and JSON type (also, go fmt issues)

--- server/observers/window-observer/lib/lib_darwin_stub.go
+++ server/observers/window-observer/lib/lib_darwin_stub.go
@@ -4,5 +4,5 @@ package lib
 
 // GetCurrentWindowMacOS is a stub used when building on non‑macOS systems.
 func GetCurrentWindowMacOS(strategy string) (*WindowInfo, error) {
-    return nil, FatalError{"darwin build tag not enabled"}
+	return nil, FatalError{"darwin build tag not enabled"}
 }
